51cb345c5bac0f5484f40ebd0495eb61ce467f22
Ankit Jain <ankitjainst@gmail.com>
2020-09-29 12:53:15 +0530

    Added Test

--- src/test/java/BitInteger/AppTest.java
+++ src/test/java/BitInteger/AppTest.java
@@ -8,7 +8,12 @@ import static org.junit.Assert.*;
 
 public class AppTest {
     @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+        int val = 19;
+        BitInteger b1 = new BitInteger();
+        b1.valueToBitInteger(val);
+
+        BitInteger b2 = new BitInteger();
+        b2.byteArrToBitInteger(b1.toByteArray(), 0);
+        assertEquals(b2.getValue(), val);
     }
 }
